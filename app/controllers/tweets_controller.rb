6b9febfa90e286127eb5d89c5a04288527aee854
supree5414 <supree@seekster.co>
2019-11-21 15:22:51 +0700

    Fixed tweets not found

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,12 @@
 class TweetsController < ApplicationController
 
   def create
-
+    @user = current_user.tweets.create(tweet_params)
   end
 
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:context)
+  end
 end
