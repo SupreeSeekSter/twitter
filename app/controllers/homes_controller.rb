1a468347d3541a347098a5968e041fed8278500b
supree5414 <supree@seekster.co>
2019-11-21 14:33:46 +0700

    Get current_user

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,10 @@ class HomesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user = current_user
+    @user = current_user.email
+  end
+
+  def current_user
+    super
   end
 end
